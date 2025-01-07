0708e5ca9f4f05b20a0de5408bb2f970d76ae31f
Vikas singh <abhirajput94781@gmail.com>
2025-01-07 20:36:22 +0530

    remove timeout in axios instance

--- src/config/makeRequests.ts
+++ src/config/makeRequests.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout: 10000,
+    // timeout: 10000,
     headers: {
         "Content-Type": "application/json",
     },
